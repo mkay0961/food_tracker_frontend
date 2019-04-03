c49d7c2f420dc79dbe181e9b58369326886066ba
mkay0961 <mkay0961@gmail.com>
2019-04-03 11:59:09 -0400

    working on add food modal

--- src/components/AddModal.js
+++ src/components/AddModal.js
@@ -1,34 +1,49 @@
-import React from 'react';
-import { Header, Modal } from 'semantic-ui-react'
+import React, {Component} from 'react';
+import { Header, Modal, Button } from 'semantic-ui-react'
+import SearchBar from './SearchBar'
+import FoodContainer from '../containers/FoodContainer'
 
-const AddModal = (props) => (
-  <div>
-    <Modal trigger={<button className="ui button">Add Food</button>}>
-      <Modal.Header>Add Food</Modal.Header>
-      <Modal.Content image>
-        <Modal.Description>
-          <Header>stuff</Header>
-          <form className="ui form">
-            <div className="field">
-              <label>First Name</label>
-              <input type="text" name="first-name" placeholder="First Name"/>
-            </div>
-            <div className="field">
-              <label>Last Name</label>
-              <input type="text" name="last-name" placeholder="Last Name"/>
-            </div>
-            <div className="field">
-              <div className="ui checkbox">
-                <input type="checkbox" tabIndex="0" className="hidden"/>
-                <label>I agree to the Terms and Conditions</label>
+class AddModal extends Component {
+  constructor(){
+    super()
+    this.state = {
+      active: false
+    }
+  }
+
+  open = () => this.setState({ active: true })
+
+  close = () => this.setState({ active: false })
+
+  render() {
+
+  const { active } = this.state
+
+   return (
+      <div>
+        <Modal open={active}  onOpen={this.open} onClose={this.close} trigger={<button className="ui button">Add Food</button>}>
+          <Modal.Header>Add Food</Modal.Header>
+          <Modal.Content >
+            <Modal.Description>
+              <div class="row">
+                <div class="column">
+                  <Header>All Food</Header>
+                  <SearchBar />
+                  <FoodContainer />
+                </div>
+                <div class="column">
+                  <Header>Food To Add</Header>
+                </div>
               </div>
-            </div>
-            <button className="ui button" type="submit">Submit</button>
-          </form>
-        </Modal.Description>
-      </Modal.Content>
-    </Modal>
-  </div>
-);
+            </Modal.Description>
+            <Modal.Actions>
+              <Button icon='check' content='Submit' onClick={this.close}/>
+            </Modal.Actions>
+          </Modal.Content>
+        </Modal>
+      </div>
+      )
+    }
+}
 
 export default AddModal;
